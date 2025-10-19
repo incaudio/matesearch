c258c1853fa17188b48ab7da09aa6a902a02cacb
incaudio <incaudio.co@gmail.com>
2025-10-19 09:13:31 +0000

    me

--- drizzle.config.d1.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  out: "./migrations/d1",
-  schema: "./server/db/schema.d1.ts",
-  dialect: "sqlite",
-  driver: "d1-http",
-});
